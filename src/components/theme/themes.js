4510aa41aa42d1acac2f2e035cfd898e3a519337
Diego Barrionuevo <diegobarrionuevo11@gmail.com>
2023-01-02 19:24:43 -0300

    git config core.ignorecase false

---
+++ src/components/theme/themes.js
@@ -0,0 +1,41 @@
+/*
+----------FUENTES
+*/
+
+const Fuentes = {
+    Ubuntu : `'Ubuntu', sans-serif;`,
+    Italiana : `'Italiana', serif;`
+}
+/*
+-------------TEMAS
+*/
+
+const LightTheme = {
+    texto: "#1f1e1d-",
+    noTexto: "white",
+    fondo: "#e4e4e4",
+    noFondo: "#1f1e1d;",
+    fuente: Fuentes,
+    blurFondo:"#00000016"
+}
+const DarkTheme = {
+    texto: "white",
+    noTexto: "#1f1e1d",
+    fondo: "#1f1e1d;",
+    noFondo: "#e4e4e4",
+    fuente: Fuentes,
+    blurFondo:"#f6f6f610"
+}
+
+
+
+/*
+.............. EXPORTACION GENERAL 
+*/
+
+const Themes = {
+    light: LightTheme,
+    dark: DarkTheme,
+}
+
+export default Themes
